0399ac99f7cad4ca6d63c4d29f4083dcf90f1270
Pranay Sarkar <pranaysarkar22@gmail.com>
2015-08-24 02:39:13 +0200

    Create phpTest.php
    php testing, working?

---
+++ phpTest.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>PHP with HTML5</title>
+ 
+<link rel="stylesheet" href="" type="text/css" />
+ 
+<!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lte IE 7]>
+  <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+<!--[if lt IE 7]>
+ 
+  <link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+</head>
+ 
+<body>
+ <header>PHP Test Page</header>
+ <br/>
+  <?php
+    echo '<p>PHP Test!</p>';
+  ?>
+  <br/>
+  <footer>Copyright @Pranay - 2015</footer>
+</body>
+</html>
+
